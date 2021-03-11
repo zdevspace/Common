ca9afa281bbab7a4a249df8365354132d23c00ab
zeisuke <zeisuke@gmail.com>
2021-03-11 15:01:22 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,10 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Common",
-            dependencies: [],
-            resources:[
-              
-            ]),
+            dependencies: []
         .testTarget(
             name: "CommonTests",
             dependencies: ["Common"]),
